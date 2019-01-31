7e72424ea958e3f54afd7ec7135f889e6d722190
nemnous <nani.botta.123@msitprogram.net>
2019-01-31 16:03:04 +0530

    "Grid Message"

--- practise/GridAngular/GridBomb/src/app/buttons/buttons.component.ts
+++ practise/GridAngular/GridBomb/src/app/buttons/buttons.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 // import { button} from './button';
 @Component({
   selector: 'app-buttons',
-  templateUrl: './buttons.component.html',
+  templateUrl: './buttons.component.html' ,
   styleUrls: ['./buttons.component.css']
 })
 export class ButtonsComponent {
   nums = [1,2,3];
   k = 0;
-  clicked() {
-    window.alert("nani");
+  @Input() bomb;
+  @Output() messageEvent = new EventEmitter<boolean>();
+  showDiv :boolean;
+  clicked(that) {
+    if(that == this.bomb) {
+      alert("Bomb");
+      document.getElementById("game");
+      this.sendMessage();
+    } else {
+      
+    }
+  }
+  sendMessage() {
+    this.messageEvent.emit(false);
   }
   constructor() { 
   }  
